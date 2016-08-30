7616356cb051bfa422e2298bd796bdbd1993dbee
sensei100 <jenanton01@gmail.com>
2016-08-30 10:10:16 -0400

    able to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,17 @@ class CommentsController < ApplicationController
     redirect_to destination_path(@destination)
   end
 
+  def destroy
+    @destination = Destination.find(params[:destination_id])
+    @comment = @destination.comments.find(params[:id])
+    @comment.destroy
+    redirect_to destination_path(@destination)
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:content)
   end
-  
+
 end
