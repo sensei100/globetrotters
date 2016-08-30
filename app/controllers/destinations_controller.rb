d5deea657287a6cb0499e15c9478ccdca1224f30
sensei100 <jenanton01@gmail.com>
2016-08-29 20:52:50 -0400

    added create and show view for destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,9 +1,24 @@
 class DestinationsController < ApplicationController
 
+  def show
+    @destination = Destination.find(params[:id])
+  end
+
   def new
   end
 
   def create
+    @destination = Destination.new(destination_params)
+    @destination.save
+    redirect_to @destination
   end
-  
+
+
+
+  private
+
+  def destination_params
+    params.require(:destination).permit(:name, :region, :country)
+  end
+
 end
