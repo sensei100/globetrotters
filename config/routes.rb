1a2a65997a4cf1b0eb0ffea14cbfe92763ea24aa
sensei100 <jenanton01@gmail.com>
2016-09-02 17:14:15 -0400

    worked on signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'registrations' }
   resources :destinations do
     resources :reviews, :comments 
   end
