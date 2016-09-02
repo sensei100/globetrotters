64fe43a2c952f50ee36791b9715ccc1ac72c80dd
sensei100 <jenanton01@gmail.com>
2016-09-02 11:50:22 -0400

    added user associations to db and created logout function

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902125512) do
+ActiveRecord::Schema.define(version: 20160902152234) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160902125512) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "review_id"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["destination_id"], name: "index_comments_on_destination_id"
