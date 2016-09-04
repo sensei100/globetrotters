c2ecde5632c688011e8ddcab0aad32abb65a19b1
sensei100 <jenanton01@gmail.com>
2016-09-04 17:48:43 -0400

    set up controller actions and have tips index and new actions working

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -4,4 +4,28 @@ class TipsController < ApplicationController
     @tips = Tip.all
   end
 
+  def show
+    @tip = Tip.find(params[:id])
+  end
+
+  def new
+    @tip = Tip.new
+  end
+
+  def create
+    @tip = Tip.new(tip_params)
+    
+    if @tip.save
+      redirect_to @tip
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def tip_params
+    params.require(:tip).permit(:name, :content)
+  end
+
 end
