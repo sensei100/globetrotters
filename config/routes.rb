5beeb5a09a92198f07958b7cbefcd8ea760da827
sensei100 <jenanton01@gmail.com>
2016-09-05 11:42:26 -0400

    changed buttons to links on destination show page and styled edit view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'registrations' }
   resources :destinations do
-    resources :reviews, :comments 
+    resources :reviews, :comments
   end
   resources :reviews
   resources :tips
