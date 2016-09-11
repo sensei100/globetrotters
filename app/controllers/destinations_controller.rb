5ab44080aacc45f73b4bbf0acd5165f28726c189
sensei100 <jenanton01@gmail.com>
2016-09-11 09:55:51 -0400

    added conditional to edit build

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -20,12 +20,16 @@ class DestinationsController < ApplicationController
 
   def edit
     @destination = Destination.find(params[:id])
-    @destination.reviews.build
+    @destination.reviews.build if !@destination.reviews.exists?
   end
 
   def create
     @destination = Destination.new(destination_params)
     @destination.reviews.first.user = current_user
+    @existing = Destination.reviews.where('content= ?', params[:content]).first
+    if !@existing.present?
+      @destination.reviews.build 
+  end
     if @destination.save
       redirect_to @destination
     else
