f26c2af6a5f969019306b24ad734a462a8751a68
sensei100 <jenanton01@gmail.com>
2016-09-12 09:50:51 -0400

    adding edit and update to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,22 @@ class ReviewsController < ApplicationController
 
   def show
     @review = Review.find(params[:id])
-    @destination = Destination.find(params[:destination_id])
+    @destination = Destination.find_by(params[:destination_id])
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    
+    if @review.user = current_user
+      @review.update(review_params)
+      redirect_to review_path(@review)
+    else
+      render 'edit', alert: "You can only edit your own tips"
+    end
   end
 
   def destroy
@@ -19,7 +34,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:rating, :content, :user_id)
+    params.require(:review).permit(:rating, :content, :user_id, :destination_id)
   end
 
 end
