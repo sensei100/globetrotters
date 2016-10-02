0310471416b222980b8d288ffb827b6c258a044c
sensei100 <jenanton01@gmail.com>
2016-10-02 14:24:58 -0400

    added attributes to serializers

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,3 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :content
 end
--- app/serializers/destination_serializer.rb
+++ app/serializers/destination_serializer.rb
@@ -1,3 +1,3 @@
 class DestinationSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :region, :country
 end
