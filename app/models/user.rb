13b5b494abaf3c99b707e23e92954b0523ad9bfa
sensei100 <jenanton01@gmail.com>
2016-10-04 12:56:17 -0400

    comments show name for facebook users

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.name = auth.info.email.split("@")[0].gsub(/[.]/, '')
       user.password = Devise.friendly_token[0,20]
     end      
   end
