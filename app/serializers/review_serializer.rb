113e7c71b23acafa3346f6249ac6972d1080b669
sensei100 <jenanton01@gmail.com>
2016-10-07 12:42:45 -0400

    generated review_user serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :rating, :content
+  attributes :id, :rating, :content, :user_id
 end
---
+++ app/serializers/review_user_serializer.rb
@@ -0,0 +1,3 @@
+class ReviewUserSerializer < ActiveModel::Serializer
+  attributes :id, :name
+end
