7191bf5fbd8f929fd6894724dc7df79ecdc40ce3
sensei100 <jenanton01@gmail.com>
2016-10-07 10:06:12 -0400

    moved js to the asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree .
+//= require destinations
+
 
 
---
+++ app/assets/javascripts/destinations.js
@@ -0,0 +1,26 @@
+
+$(function () {
+  $(".js-next").on("click", function(e) {
+    e.preventDefault()
+    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+    $.get("/reviews/" + nextId + ".json", function(data) {
+      $("#rating").text("Rating: " + data["rating"]);
+      $("#reviewContent").text(data["content"]);
+      // re-set the id to current on the link
+      $(".js-next").attr("data-id", data["id"]);
+    });
+  });
+
+
+$("a.loadComments").on("click", function(e){
+ 
+  $.get(this.href).success(function(json){
+    var $ol = $("div.commentDiv ol")
+    $ol.html("") 
+    json.forEach(function(comment) {
+      $ol.append("<li>" + comment.user.name + ": <br>" + comment.content + "</li><br>");
+    })
+    })
+    e.preventDefault();
+  })
+  }); 
