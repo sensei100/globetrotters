8b80f5f463b75eb5e09575cc46c5c69291ed99fc
sensei100 <jenanton01@gmail.com>
2016-10-16 21:30:21 -0400

    created review object in js

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,3 +1,39 @@
+function Review(data) {
+  this.id = data.id;
+  this.content = data.content;
+  this.user = data.user;
+  this.authenticity_token =  $("input[name='authenticity_token']").val();
+}
+
+Review.prototype.renderDisplay = function() {
+  alert("Your review was created");
+  var html = "";
+  html += "<p>" + this.user.name + ": <br>" + this.content + "</p><br>";
+}
+
+$(function() { 
+  $("form#new_comment").on("submit", function(event) {
+    event.preventDefault();
+    var $form = $(this);
+    var action = $form.attr("action");
+    var params = $form.serialize();
+
+    $.ajax({
+      url: action,
+      data: params,
+      dataType: "json",
+      method: "POST"
+    })
+    .success(function(json) {
+      var review = new Review(json);
+      var reviewDisplay = review.renderDisplay();
+
+      $('#lastComment p').append(commentDisplay);
+
+    })
+  })
+})
+
 $(function () {
   $(".js-more").on('click', function() {
     var id = $(this).data("id");
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :review_count
+  attributes :id, :name
 
   has_many :comments, :reviews
   
